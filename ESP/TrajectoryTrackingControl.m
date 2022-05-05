63f92e6196b18a3f6c61fbdfc306831efe04c9ec
Alessandro Tavoso <atavoso@gmail.com>
2022-05-05 12:15:02 +0200

    Riorganizzazione cartella ESP e creazione versione Sim 2021a

--- ESP/TrajectoryTrackingControl.m
+++
@@ -1,8 +0,0 @@
-function veq = TrajectoryTrackingControl(xref,dotxref)
-
-global Klqr A Iz
-K = -Klqr;
-B2eq = [0; 1/Iz];
-Cstar = [0 1];
-veq = -inv(Cstar*inv(A+B2eq*K)*B2eq)*Cstar*(xref-inv(A+B2eq*K)*dotxref);
-end
